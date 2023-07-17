92e5f6773edf8395d2000df74dcca4437c3636c1
NittaiPe24-meet <nittai.peri@gmail.com>
2023-07-17 09:46:23 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -2,15 +2,25 @@
 
 
 def create_youtube_video(title = input("title: "), description = input("description: ")):
-	video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
-	return video
+	youtube_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+	return youtubr_video
 
-def like(videoA = input()):
-	if videoA["likes"]:
+def like(videoA = input("gimmie da dictionary: ")):
+	if ("likes" in videoA):
 		videoA["likes"] += 1
+		print(videoA["likes"])
 		return (videoA["likes"])
 
-def dislike(videoB = input()):
+def dislike(videoB = input("gimmie da dictionary; ")):
 	if videoB["dislikes"]:
 		videoB["dislikes"] += 1
+		print(videoB["dislikes"])
 		return (videoB["dislikes"])
+
+def add_comment(video, user, text):
+	video["comments"][user] = text
+	return video
+
+
+
+
